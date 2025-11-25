23e065d10ce61a3c3299576a327d0e38087ffcdb
Attila Somogyvari <somogyvari.attila16@gmail.com>
2025-11-25 21:52:45 +0100

    Tovabbi /info endpoint feature hozzaadasa alapveto app metaadatok megjeleniteseert

--- app.py
+++ app.py
@@ -6,5 +6,9 @@ app = Flask(__name__)
 def hello():
     return "Hello (Cruel) DevOps World!"
 
+@app.route("/info")
+def info():
+    return "App: Hello (Cruel) DevOps World – Verzio 1.0.0"
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080)
